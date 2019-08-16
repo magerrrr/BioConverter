a5d915782341830c479b66189ddc190fbd704e35
Kirill Mager <kiruha24@mail.ru>
2019-08-16 22:55:17 +0300

    Initial Commit

---
+++ BioConverter/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  BioConverter
+//
+//  Created by Kirill on 8/16/19.
+//  Copyright © 2019 Kirill. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
