79bf6c55e692ee8c214c7cc682384a495a953dd2
Kirill Mager <kiruha24@mail.ru>
2019-08-19 18:02:44 +0300

    +4 args

--- BioConverter/main.c
+++ BioConverter/main.c
@@ -7,42 +7,38 @@
 //
 #include <stdio.h>
 #include <stdlib.h>
-
-int main(void) {
-    // float glucose_mmol_l = 1;
-    float glucose_mg_dl = 18.0156;
+int main(int argc, char *argv[]) {
+    int16_t biomarker;
+    int16_t convertFrom;
+    int16_t convertTo;
+    double value;
     
+    float glucose_mg_dl = 18.0156;
     printf("Биоконвертер: \n");
     printf("\nВведите число для выполнения операции: \n");
     printf("Перевод Ммоль/Л в Мг/дЛ: 1\n");
     printf("Перевод Мг/дЛ в Ммоль/Л: 2\n");
-
     int number = 0;
-    float value = 0.0;
-    float result = 0.0;
-    
+    float val = 0.0;
+    double result = 0.0;
     scanf("%d", &number);
     printf("Введите сумму: \n");
-    scanf("%f", &value);
-    
+    scanf("%f", &val);
     switch (number) {
         case 1:
-            result = value * glucose_mg_dl;
+            result = val * glucose_mg_dl;
             printf("Значение в мг/дл: %.2f\n", result);
             break;
-            
         case 2:
-            result = value / glucose_mg_dl;
+            result = val / glucose_mg_dl;
             printf("Значение в Ммоль/л: %.2f\n", result);
             break;
-            
         default:
             printf("Вы ввели не корректное число!");
             break;
     }
     return 0;
 }
-
 
 // ФУНКЦИЯ В СИ:
 // ТипВозвращаемогоЗначения ИмяФункции(СписокФормальныхАргументов)
