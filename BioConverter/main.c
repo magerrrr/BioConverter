7d1a32555c901209d8983826dc7b5940d0e28426
Kirill Mager <kiruha24@mail.ru>
2019-08-19 23:05:53 +0300

    Update main.c (3 Array, 1 Function)
    Added Array biomarker
    Added Array insulin_units
    Added Array glucose_units
    Added function convert

--- BioConverter/main.c
+++ BioConverter/main.c
@@ -7,43 +7,53 @@
 //
 #include <stdio.h>
 #include <stdlib.h>
-int main(int argc, char *argv[]) {
-    int16_t biomarker;
-    int16_t convertFrom;
-    int16_t convertTo;
-    double value;
+
+char *biomarker[2] = {
+    "Insulin", //0
+    "Glucose", //1
+};
+
+int insulin_units[15] = { // 0
+    1, //mmol/l
+    1000,
+    1000000,
+    1000000000,
+    0.180156,
+    18.0156, //mg/dl
+    180.156,
+    180.156
+};
+
+int glucose_units[15] = { // 1
+    1, //mmol/l
+    1000,
+    1000000,
+    1000000000,
+    0.180156,
+    18.0156, //mg/dl
+    180.156,
+    180.156
+};
+
+double convert(int16_t indexOfBiomarker, int16_t convertFrom, int16_t convertTo, int value) {
+    printf("%s\n", biomarker[indexOfBiomarker]);
+    printf("%d\n", glucose_units[convertFrom]);
+    printf("%d\n", glucose_units[convertTo]);
     
-    float glucose_mg_dl = 18.0156;
-    printf("Биоконвертер: \n");
-    printf("\nВведите число для выполнения операции: \n");
-    printf("Перевод Ммоль/Л в Мг/дЛ: 1\n");
-    printf("Перевод Мг/дЛ в Ммоль/Л: 2\n");
-    int number = 0;
-    float val = 0.0;
-    double result = 0.0;
-    scanf("%d", &number);
-    printf("Введите сумму: \n");
-    scanf("%f", &val);
-    switch (number) {
-        case 1:
-            result = val * glucose_mg_dl;
-            printf("Значение в мг/дл: %.2f\n", result);
-            break;
-        case 2:
-            result = val / glucose_mg_dl;
-            printf("Значение в Ммоль/л: %.2f\n", result);
-            break;
-        default:
-            printf("Вы ввели не корректное число!");
-            break;
-    }
+    double result = value * glucose_units[convertTo]/glucose_units[convertFrom];
+    return result;
+}
+
+int main(int argc, char *argv[]) {
+    printf("Значение result: %f\n", convert(0, 0, 1, 5) );
     return 0;
 }
 
-// ФУНКЦИЯ В СИ:
-// ТипВозвращаемогоЗначения ИмяФункции(СписокФормальныхАргументов)
-// {
-//    ТелоФункции;
-//    ...
-//    return(ВозвращаемоеЗначение);
-// }
+//typedef struct Converter {
+// int value;
+// struct {
+// int id;
+// char *name;
+// } make;
+// char *name;
+// } DB;
